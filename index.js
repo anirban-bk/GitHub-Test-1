b5923c7de9889fcaeae65298b4f786fb07fabfe2
anirbanb <anirbanb@eidesign.net>
2023-12-13 18:48:44 +0530

    this is second commit - branch named branch1

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const express = require('express');
 const app =express();
 
 const port = 8220;
-app.listen(port, console.log(`the app is listening on port ${port}`));
+app.listen(port, console.log(`the app is listening on portt ${port}`));
